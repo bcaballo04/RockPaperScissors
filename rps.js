4247f38eb58fe87297e5a00e5efa20c56a67b62c
bcaballo04 <bcaballo@umich.edu>
2023-06-19 17:51:12 -0400

    Working version of buttons and display value

--- rps.js
+++ rps.js
@@ -1,3 +1,82 @@
+const rockb = document.createElement("button");
+rockb.textContent = "Rock!"
+rockb.classList.add('rock');         
+document.body.appendChild(rockb);
+
+const paperb = document.createElement("button");
+paperb.textContent = "Paper!"
+paperb.classList.add('paper');    
+document.body.appendChild(paperb);
+
+const scissorsb = document.createElement("button");
+scissorsb.textContent = "Scissors!"
+scissorsb.classList.add('scissors');    
+document.body.appendChild(scissorsb);
+
+const display = document.createElement("div");
+display.textContent = ` `;
+document.body.appendChild(display);
+
+let outcome = 0;
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach(function (button){
+    button.addEventListener("click", function (e){
+        outcome = playRound(button.className, getComputerChoice())
+        display.textContent = outcome;
+    });
+});
+
+
+
+/*rockb.addEventListener("click", function (e){
+    outcome = playRound("Rock", getComputerChoice())
+});
+
+paperb.addEventListener("click", function (e){
+   outcome = playRound("Paper", getComputerChoice())
+});
+
+scissorsb.addEventListener("click", function (e){
+    outcome = playRound("Scissors", getComputerChoice())
+});
+
+
+function game(){
+    let playerScore = 0;
+    let compScore = 0;
+
+    while (playerScore < 5 && compScore < 5){
+
+    if (outcome == 1){
+        display.textContent = "You win because " + playerSelection + " beats " + computerSelection + "!"
+        playerScore++
+    }
+
+    if (outcome == 2){
+        display.textContent = "You tie! Same option!"
+    }
+
+    if (outcome == 3){
+        display.textContent = "You lose because " + computerSelection + " beats " + playerSelection + "!"
+        compScore++
+    }
+
+    if (outcome == 4){
+        display.textContent = "Choose a correct item!"
+    }
+}
+
+if (playerScore == 5){
+    display.textContent = "5 rounds won! You win!!"
+}
+
+if (compScore == 5){
+    display.textContent = "5 rounds lost! Computer wins!!"
+}
+
+} */
+
 function getComputerChoice(){
     let random = Math.floor(Math.random() * 3);
 
@@ -12,9 +91,9 @@ function getComputerChoice(){
     if (random == 2){
         return "paper"
     }
-}
+} 
 
-function playRound(playerSelection, computerSelection, playerScore, compScore){
+function playRound(playerSelection, computerSelection){
     let player = playerSelection.toLowerCase();
 
     if ((player == "rock" && computerSelection == "scissors") || (player == "paper" && computerSelection == "rock") || (player == "scissors" && computerSelection == "paper")){
@@ -35,44 +114,5 @@ function playRound(playerSelection, computerSelection, playerScore, compScore){
 }
 
 
-function game(){
-    let playerScore = 0;
-    let compScore = 0;
-
-    while (playerScore < 5 && compScore < 5){
-
-    let playerSelection = prompt("Choose rock, paper, or scissors!");
-    let computerSelection = getComputerChoice();
-    let outcome = playRound(playerSelection, computerSelection);
 
 
-    if (outcome == 1){
-        console.log("You win because " + playerSelection + " beats " + computerSelection + "!")
-        playerScore++
-    }
-
-    if (outcome == 2){
-        console.log("You tie! Same option!")
-    }
-
-    if (outcome == 3){
-        console.log( "You lose because " + computerSelection + " beats " + playerSelection + "!")
-        compScore++
-    }
-
-    if (outcome == 4){
-        console.log("Choose a correct item!")
-    }
-}
-
-if (playerScore == 5){
-    console.log("5 rounds won! You win!!")
-}
-
-if (compScore == 5){
-    console.log("5 rounds lost! Computer wins!!")
-}
-
-}
-
-game();
